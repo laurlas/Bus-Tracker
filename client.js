cba816d92a089d5d45addafe0d8fc7033e8f194c
laurlas <laurentiulas95@gmail.com>
2017-05-30 22:41:43 +0300

    first commit

---
+++ client.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    let ws = io("client");
+    ws.addEventListener('error', function (event) {
+        alert('Server is closed!');
+    });
+    ws.addEventListener('close', function (event) {
+        alert('Connection closed!');
+    });
+    $(document).on('click', '#submit', function () {
+        let request = {
+            busNr: $('#bus-nr').val()
+        };
+        ws.send(JSON.stringify(request));
+    });
+    let markersArray=[];
+    ws.onmessage = function (response) {
+        console.log(response.data);
+        locations = JSON.parse(response.data);
+        for (let i = 0; i < markersArray.length; i++ ) {
+            markersArray[i].setMap(null);
+        }
+        markersArray.length = 0;
+        for (let loc in locations) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[loc].lat, locations[loc].lng),
+                map: map,
+                title: $('#bus-nr').val()
+            });
+            markersArray.push(marker);
+        }
+    };
+	
+    function geolocate() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                let geolocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                ws.send(JSON.stringify(geolocation));
+            });
+        }
+    }
+});
+
---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
+const app = express();
+
+const server = http.createServer(app);
+const ws = new WebSocket.Server({server});
+let clients = [];
+let busses = [];
+
+app.get('/bus',function(req,res){
+    res.sendFile('bus.html');
+});
+
+app.get('/',function(req,res){
+    res.sendFile('client.html');
+});
+
+ws.on('connection', function connection(ws) {
+    let bus = false;
+    if (ws.protocol) {
+        prot = ws.protocol.split('-');
+        if (prot.length === 2) {
+            ws.busNr = prot[1];
+            busses.push(ws);
+            bus = true;
+        }
+        else {
+            clients.push(ws);
+        }
+    }
+    else {
+        clients.push(ws);
+    }
+    ws.on('message', function incoming(message) {
+        let data = JSON.parse(message);
+        if (bus === true) {
+            ws.lat = data.lat;
+            ws.lng = data.lng;
+        }
+        else {
+            let bussesSearch = {};
+            for (let i = 0; i < busses.length; i++) {
+                if (busses[i].busNr === data.busNr) {
+                    bussesSearch[i] = {lat: busses[i].lat, lng: busses[i].lng}
+                }
+            }
+            ws.send(JSON.stringify(bussesSearch));
+        }
+    });
+});
+var port = process.env.PORT || 1337;
+server.listen(port);
